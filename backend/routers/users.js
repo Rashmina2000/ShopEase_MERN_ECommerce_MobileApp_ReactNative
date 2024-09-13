8b49f1c355fcba7c65269c704a27c5ab0981f448
Rashmina Amarasinghe <shasheenrashmina@gmail.com>
2024-09-13 20:33:33 +0530

    user create with pwHash

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -1,5 +1,6 @@
 const { User } = require("../models/user");
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
@@ -9,6 +10,26 @@ router.get(`/`, async (req, res) => {
     res.status(500).json({ success: false });
   }
   res.send(userList);
+});
+
+router.post(`/`, async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+  user = await user.save();
+  if (!user) {
+    return res.status(400).send("User cannot be register");
+  }
+  res.send(user);
 });
 
 module.exports = router;
